b36ed0e1b0bdc97f7172d94f99a31ca85aa56b91
midannii <48229457+midannii@users.noreply.github.com>
2020-11-17 16:11:12 +0900

    Create svd.cpp

---
+++ codes/week10/svd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <eigen/Dense>
+#include <eigen/LU>
+using namespace Eigen;
+using namespace std;
+
+int main() {
+    Matrix <float, 3, 2> A;
+    A << 1, -1, -2, 2, 2, -2;
+    cout << A << endl;
+    Matrix <float, 2, 3> AT = A.transpose();
+    cout << AT << endl;
+    
+    Matrix3d AAT; Matrix3d ATA;
+    AAT = A*AT; // (3x3)
+    ATA = AT*A; // (2x2)
+    EigenSolver<Matrix2d> Aeigen;
+    EigenSolver<Matrix3d> Aeigen2;
+    Aeigen.compute(ATA);
+    Aeigen2.compute(AAT);
+    
+    Matrix3d U = Aeigen2.pseudoEigenvectors();
+    Matrix2d V = Aeigen.pseudoEigenvectors();
+    Matrix <float, 3, 2> sigma;
+    sigma = pseudoEigenvalueMatrix();
+    
+    cout << "U * sigma * V = " << U * sigma * V << endl;
+    return 0;
+}
